eaf9fffd1e5ac414868a935536fbceb698a7672d
Carlos Marfil <carlos.marfil.r@gmail.com>
2022-10-09 13:59:00 +0100

    FizzBuzz Solved

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,15 @@
-const fizzBuzz = (number) => {};
-
+const fizzBuzz = (number) => {
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      return "FizzBuzz";
+    }
+    if (i % 3 === 0) {
+      return "Fizz";
+    }
+    if (i % 5 === 0) {
+      return "Buzz";
+    }
+    return i;
+  }
+};
 module.exports = fizzBuzz;
