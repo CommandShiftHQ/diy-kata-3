53255e30984ec2b3b47ff02ba7b312cdc2496c83
Carlos Marfil <carlos.marfil.r@gmail.com>
2022-10-09 14:39:04 +0100

    Test and Solution for Boolean words done

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -2,15 +2,9 @@ const fizzBuzz = (number) => {
   const isDivisableBy3 = number % 3 === 0;
   const isDivisableBy5 = number % 5 === 0;
 
-  if (isDivisableBy3 && isDivisableBy5) {
-    return "FizzBuzz";
-  }
-  if (isDivisableBy3) {
-    return "Fizz";
-  }
-  if (isDivisableBy5) {
-    return "Buzz";
-  }
-  return number;
-};
+isDivisableBy3 && isDivisableBy5 ? "FizzBuzz"
+    : isDivisableBy3 ? "Fizz"
+    : isDivisableBy5 ? "Buzz"
+    : number;
+}
 module.exports = fizzBuzz;
